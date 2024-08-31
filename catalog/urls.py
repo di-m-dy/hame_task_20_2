3b238dd3f8a4a2723bc1a9d231a9be826be83667
di-m-dy <dimdy11.11@gmail.com>
2024-08-31 23:29:31 +0200

    add pagination of category

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from catalog.views import index, contacts, product
+from catalog.views import index, contacts, product, products
 
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:product_id>', product, name="product")
+    path('product/<int:product_id>', product, name="product"),
+    path('products/', products, name="products")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,7 @@ def index(request):
     """
     Контроллер для главной страницы
     """
-    product_data = list(Product.objects.all())[-6:]
+    product_data = list(Product.objects.all())[-3:]
     return render(request, 'catalog/index.html', {"products": product_data})
 
 
@@ -29,9 +29,65 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', {"contacts": contacts_data})
 
 
+def products(request):
+    category_id = request.GET.get("category_id")
+    if category_id:
+        category_data = get_object_or_404(Category, id=category_id)
+        product_data = Product.objects.filter(category_id=category_id)
+
+    else:
+        category_data = None
+        product_data = Product.objects.all()
+
+    context = {
+        "category": category_data,
+        "products": product_data,
+        "categories": Category.objects.all()
+    }
+    return render(request, 'catalog/products.html', context=context)
+
+
 def product(request, product_id):
     product_data = get_object_or_404(Product, id=product_id)
     context = {
         "product": product_data
     }
     return render(request, 'catalog/product.html', context=context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        category = request.POST.get("category")
+        product_name = request.POST.get("product_name")
+        product_description = request.POST.get("product_description")
+        product_image = request.POST.get("product_image")
+        product_price = request.POST.get("product_image")
+        warning_text = ''
+        if all(
+            [
+                product_name,
+                product_description,
+                product_image,
+                product_price
+            ]
+        ):
+            if not product_price.is_digit():
+                warning_text = "В поле цена необходимо ввести числовое значение"
+                return render(request, 'catalog/add_product.html', context={"warning": warning_text})
+
+            product_data = Product(
+                name=product_name,
+                category=category,
+                description=product_description,
+                image=product_image,
+                price=product_price
+            )
+            product_data.save
+            return render(
+                request,
+                'catalog/success_adding_product.html',
+                context={"product": product_data}
+            )
+    else:
+        warning_text = 'Неверный ввод'
+    return render(request, 'catalog/add_product.html', context={"warning": warning_text})
